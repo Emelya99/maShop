fa3301e8b6d73532f73a96e96e351da272ea800c
artur.emelyanov99 <artur.emelyanov99@gmail.com>
2022-08-01 17:12:13 +0300

    Fixed layout product

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -9,26 +9,30 @@ const Product = ({ title, imgUrl, sizes, price }) => {
 
   return (
     <div className={styles.product}>
-      <a href="/">
-        <img src={imgUrl} alt="product" />
-      </a>
-      <a className={styles.title} href="/">
-        {title}
-      </a>
-      <div className={styles.variables}>
-        {sizes.map((size, index) => (
-          <p
-            onClick={() => setSizeActive(index)}
-            className={sizeActive === index ? styles.active : ''}>
-            {sizesProduct[size]}
-          </p>
-        ))}
+      <div className={styles.productTop}>
+        <a href="/">
+          <img src={imgUrl} alt="product" />
+        </a>
+        <a className={styles.title} href="/">
+          {title}
+        </a>
       </div>
-      <div className={styles.bottom}>
-        <p>From {price} $</p>
-        <button onClick={() => setAddCartCount((prev) => prev + 1)}>
-          + Add<span>{addCartCount}</span>
-        </button>
+      <div>
+        <div className={styles.variables}>
+          {sizes.map((size, index) => (
+            <p
+              onClick={() => setSizeActive(index)}
+              className={sizeActive === index ? styles.active : ''}>
+              {sizesProduct[size]}
+            </p>
+          ))}
+        </div>
+        <div className={styles.bottom}>
+          <p>From {price} $</p>
+          <button onClick={() => setAddCartCount((prev) => prev + 1)}>
+            + Add<span>{addCartCount}</span>
+          </button>
+        </div>
       </div>
     </div>
   );
