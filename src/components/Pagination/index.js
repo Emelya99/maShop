fb9c6645c2a748be611cb14ec58241c50241c4bc
artur.emelyanov99 <artur.emelyanov99@gmail.com>
2022-08-02 11:00:12 +0300

    Deleted trash on Pagination component

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -1,14 +1,9 @@
-// import React from 'react';
 import styles from './Pagination.module.scss';
 import ReactPaginate from 'react-paginate';
 
 import Icon from '../IconsGenerator';
 
 const Pagination = ({ onChangePage, countPage }) => {
-  // const [page, setPage] = React.useState(0);
-
-  // const pageCount = ['1', '2', '3'];
-
   return (
     <ReactPaginate
       breakLabel="..."
@@ -20,23 +15,6 @@ const Pagination = ({ onChangePage, countPage }) => {
       pageCount={countPage}
       renderOnZeroPageCount={null}
     />
-
-    // <ul className={styles.pagination}>
-    //   <li>
-    //     <Icon name="arrow-left" />
-    //   </li>
-    //   {pageCount.map((item, index) => (
-    //     <li
-    //       key={index}
-    //       onClick={() => setPage(index)}
-    //       className={page === index ? styles.active : ''}>
-    //       {item}
-    //     </li>
-    //   ))}
-    //   <li>
-    //     <Icon name="arrow-right" />
-    //   </li>
-    // </ul>
   );
 };
 
