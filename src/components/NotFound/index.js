fc5ee2511991709059994db3b528025e432cfde7
artur.emelyanov99 <artur.emelyanov99@gmail.com>
2022-08-04 16:25:27 +0300

    added page not found

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,10 +1,14 @@
 import Title from '../Title';
 import styles from './NotFound.module.scss';
+import { Link } from 'react-router-dom';
 
 const Cart = () => {
   return (
     <div className={styles.wrapper}>
       <Title title="Not found page" />
+      <Link to="/" className="btn">
+        Go menu
+      </Link>
     </div>
   );
 };
