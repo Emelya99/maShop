0f2206cd072f28ab1497352994650720808b727a
artur.emelyanov99 <artur.emelyanov99@gmail.com>
2022-08-23 10:49:46 +0300

    Added Outlet

--- src/App.js
+++ src/App.js
@@ -3,27 +3,20 @@ import { Routes, Route } from 'react-router-dom';
 import 'normalize.css';
 import './style.scss';
 
-import Header from './components/Header';
-import Footer from './components/Footer';
+import MainLayout from './layouts/MainLayout';
 import Home from './pages/Home';
 import CartPage from './pages/CartPage';
 import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <main className="main">
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<CartPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </div>
-      </main>
-      <Footer />
-    </div>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
   );
 }
 
--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const MainLayout = () => {
-  return <div>MainLayout</div>;
+  return (
+    <div className="app">
+      <Header />
+      <main className="main">
+        <div className="container">
+          <Outlet />
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default MainLayout;
