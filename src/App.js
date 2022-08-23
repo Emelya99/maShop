efabad923cb8a3fe5e00fd80deda8cf2e440a0a4
artur.emelyanov99 <artur.emelyanov99@gmail.com>
2022-08-23 13:54:22 +0300

    Added SingleProductPage in Routes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './style.scss';
 import MainLayout from './layouts/MainLayout';
 import Home from './pages/Home';
 import CartPage from './pages/CartPage';
+import SingleProductPage from './pages/SingleProductPage';
 import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Route path="/" element={<MainLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<CartPage />} />
+        <Route path="/product/:id" element={<SingleProductPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
---
+++ src/pages/SingleProductPage.js
@@ -0,0 +1,7 @@
+import SingleProduct from '../components/SingleProduct/SingleProduct';
+
+const SingleProductPage = () => {
+  return <SingleProduct />;
+};
+
+export default SingleProductPage;
