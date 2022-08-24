192221d93b3248743b333ff54d35dd089be9084f
artur.emelyanov99 <artur.emelyanov99@gmail.com>
2022-08-24 16:46:01 +0300

    added component similarProducts

---
+++ src/components/SimilarProducts/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import styles from './SimilarProducts.module.scss';
+
+import Title from '../Title';
+import Product from '../Product';
+import Products from '../Products';
+
+import { useSelector } from 'react-redux';
+import { productSelector } from '../../redux/slices/productSlice';
+
+const SimilarProducts = ({ products, id }) => {
+  const { similarLimitPage } = useSelector(productSelector);
+
+  const productRender = products
+    .filter((obj) => obj.id !== id)
+    .sort(() => Math.random() - Math.random())
+    .slice(0, similarLimitPage)
+    .map((product) => <Product key={product.id} {...product} />);
+
+  return (
+    <div className={styles.wrapper}>
+      <Title title="Similar products" />
+      <Products productRender={productRender} limitPage={similarLimitPage} />
+    </div>
+  );
+};
+
+export default SimilarProducts;
