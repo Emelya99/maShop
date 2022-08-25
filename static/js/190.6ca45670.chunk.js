1328d6a1ae3beb04a9b7762739e65b7d931459e9
artur.emelyanov99 <artur.emelyanov99@gmail.com>
2022-08-25 11:08:51 +0300

    Updates

---
+++ static/js/190.6ca45670.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmarket_practice_3=self.webpackChunkmarket_practice_3||[]).push([[190],{190:function(e,t,n){n.r(t),n.d(t,{default:function(){return i}});n(2791);var r=n(3081),c="NotFound_wrapper__YTg0C",u=n(3504),a=n(184),s=function(){return(0,a.jsxs)("div",{className:c,children:[(0,a.jsx)(r.Z,{title:"Not found page"}),(0,a.jsx)(u.rU,{to:"/",className:"btn",children:"Go menu"})]})},i=function(){return(0,a.jsx)(s,{})}}}]);
+//# sourceMappingURL=190.6ca45670.chunk.js.map
